87e249e0b40c908077c2ec9216453b79e63c5638
pagzamaphone <anthony.pagnotta@gmail.com>
2019-10-24 19:32:08 -0400

    added some clarification for the comments

--- lambda/helloworldlambda.py
+++ lambda/helloworldlambda.py
@@ -5,10 +5,11 @@ import json
 print('Loading function')
 
 
+
 def lambda_handler(event, context):
     #print("Received event: " + json.dumps(event, indent=2))
     print("value1 = " + event['key1'])
     print("value2 = " + event['key2'])
     print("value3 = " + event['key3'])
-    return event['key1']  # Echo back the first key value
+    return event['key1']  # Echo back the first key value cause im awesome
     #raise Exception('Something went wrong')
